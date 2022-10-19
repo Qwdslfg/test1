208c9f7420c61243c7f788937bf7dce29767d35a
Qwdslfg <102227316+Qwdslfg@users.noreply.github.com>
2022-10-19 22:59:54 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+
+white:1
+
+red:2
+
+blue:3
+
+"""
+CarPark = 0
+huskylens.init_i2c()
+huskylens.init_mode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
+
+def on_forever():
+    global CarPark
+    servos.P0.set_angle(0)
+    huskylens.request()
+    if huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        servos.P0.set_angle(90)
+        CarPark = CarPark + 1
+    else:
+        if huskylens.is_appear(2, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+            servos.P0.set_angle(90)
+            CarPark = CarPark + 1
+        else:
+            if huskylens.is_appear(3, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+                servos.P0.set_angle(90)
+                CarPark = CarPark + 1
+    basic.pause(3000)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,25 @@
+let CarPark = 0
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
+/**
+ * white:1
+ * 
+ * red:2
+ * 
+ * blue:3
+ */
+basic.forever(function () {
+    servos.P0.setAngle(0)
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        servos.P0.setAngle(90)
+        CarPark = CarPark + 1
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        servos.P0.setAngle(90)
+        CarPark = CarPark + 1
+    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        servos.P0.setAngle(90)
+        CarPark = CarPark + 1
+    }
+    basic.pause(3000)
+})
