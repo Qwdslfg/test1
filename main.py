8c182d7c7d29dc8fa71baca3747f33ae94f5c47b
Qwdslfg <102227316+Qwdslfg@users.noreply.github.com>
2022-10-19 23:52:42 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+CarPark = 0
+huskylens.init_i2c()
+huskylens.init_mode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
 """
 
 white:1
@@ -7,9 +10,6 @@ red:2
 blue:3
 
 """
-CarPark = 0
-huskylens.init_i2c()
-huskylens.init_mode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
 
 def on_forever():
     global CarPark
@@ -18,13 +18,11 @@ def on_forever():
     if huskylens.is_appear(1, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
         servos.P0.set_angle(90)
         CarPark = CarPark + 1
-    else:
-        if huskylens.is_appear(2, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
-            servos.P0.set_angle(90)
-            CarPark = CarPark + 1
-        else:
-            if huskylens.is_appear(3, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
-                servos.P0.set_angle(90)
-                CarPark = CarPark + 1
+    elif huskylens.is_appear(2, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        servos.P0.set_angle(90)
+        CarPark = CarPark + 1
+    elif huskylens.is_appear(3, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        servos.P0.set_angle(90)
+        CarPark = CarPark + 1
     basic.pause(3000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -9,17 +9,18 @@ huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
  * blue:3
  */
 basic.forever(function () {
+    CarPark = 10
     servos.P0.setAngle(0)
     huskylens.request()
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         servos.P0.setAngle(90)
-        CarPark = CarPark + 1
+        CarPark = CarPark - 1
     } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         servos.P0.setAngle(90)
-        CarPark = CarPark + 1
+        CarPark = CarPark - 1
     } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
         servos.P0.setAngle(90)
-        CarPark = CarPark + 1
+        CarPark = CarPark - 1
     }
     basic.pause(3000)
 })
