f8a31a3b05973041c7dde102db2ef8ddbdb48f1c
Qwdslfg <102227316+Qwdslfg@users.noreply.github.com>
2022-10-25 17:32:27 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,65 @@
-let CarPark = 0
+/**
+ * huskylens p6
+ * 
+ * 紅綠燈 p1
+ * 
+ * 馬達 p2
+ */
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
+let CarPark = 10
+SmartCity.turn_servo(0, AnalogPin.P2)
 /**
+ * 感應門
+ * 
  * white:1
  * 
  * red:2
  * 
  * blue:3
  */
+/**
+ * 車位情況
+ * 
+ * red=0
+ * 
+ * yellow=1-4
+ * 
+ * green=5-10
+ */
 basic.forever(function () {
-    CarPark = 10
-    servos.P0.setAngle(0)
     huskylens.request()
-    if (huskylens.isAppear(0, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        servos.P0.setAngle(90)
-        CarPark = CarPark - 1
-    } else if (huskylens.isAppear(0, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        servos.P0.setAngle(90)
-        CarPark = CarPark - 1
-    } else if (huskylens.isAppear(0, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        servos.P0.setAngle(90)
-        CarPark = CarPark - 1
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock) || (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock) || huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock))) {
+        if (CarPark >= 1) {
+            SmartCity.turn_servo(90, AnalogPin.P2)
+            basic.pause(3000)
+            CarPark += -1
+            SmartCity.turn_servo(0, AnalogPin.P2)
+        }
     }
-    basic.pause(3000)
+    basic.showNumber(CarPark)
+    if (CarPark > 5) {
+        SmartCity.control_traffic_light(
+        false,
+        false,
+        true,
+        AnalogPin.P1
+        )
+    } else {
+        if (CarPark == 0) {
+            SmartCity.control_traffic_light(
+            true,
+            false,
+            false,
+            AnalogPin.P1
+            )
+        } else {
+            SmartCity.control_traffic_light(
+            false,
+            true,
+            false,
+            AnalogPin.P1
+            )
+        }
+    }
 })
