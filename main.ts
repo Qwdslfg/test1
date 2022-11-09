9d4cf75b00b4fe26e21bd237ad49cdbe879b8f9c
Qwdslfg <102227316+Qwdslfg@users.noreply.github.com>
2022-11-09 16:29:43 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,19 @@
  * 
  * 馬達 p2
  */
+WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
+    basic.showIcon(IconNames.Yes)
+})
+WiFiIoT.on_wifi_disconnect(function (Error_code) {
+    basic.showIcon(IconNames.No)
+})
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 let CarPark = 10
+let temp = 25
 SmartCity.turn_servo(0, AnalogPin.P2)
+WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
+WiFiIoT.setWifi("iots", "12345678")
 /**
  * 感應門
  * 
@@ -28,6 +37,17 @@ SmartCity.turn_servo(0, AnalogPin.P2)
  * green=5-10
  */
 basic.forever(function () {
+    WiFiIoT.sendThingspeak(
+    "74ZCT5KZJ36EUBZQ",
+    CarPark,
+    temp
+    )
+    temp = SmartCity.readData(SmartCity.DHT11dataType.temperature, DigitalPin.P2)
+    if (temp == 0) {
+        temp = 27
+    } else {
+        temp += -2
+    }
     huskylens.request()
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock) || (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock) || huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock))) {
         if (CarPark >= 1) {
