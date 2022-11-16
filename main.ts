c498bc2bf9280c551fb640e98f1337fd2f509b2a
Qwdslfg <102227316+Qwdslfg@users.noreply.github.com>
2022-11-16 17:51:40 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,8 @@ huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 let CarPark = 10
 let temp = 25
-SmartCity.turn_servo(0, AnalogPin.P2)
+let count = 0
+SmartCity.turn_servo(90, AnalogPin.P2)
 basic.forever(function () {
     if (CarPark > 5) {
         SmartCity.control_traffic_light(
@@ -37,20 +38,28 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (SmartCity.read_distance_sensor(SmartCity.DistanceUnit.Centimeters, DigitalPin.P13, DigitalPin.P14) < 10) {
-        CarPark += 1
-    }
-})
-basic.forever(function () {
     huskylens.request()
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock) || (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock) || huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock))) {
         if (CarPark >= 1) {
-            SmartCity.turn_servo(90, AnalogPin.P2)
+            SmartCity.turn_servo(0, AnalogPin.P2)
             basic.pause(3000)
             CarPark += -1
-            SmartCity.turn_servo(0, AnalogPin.P2)
+            SmartCity.turn_servo(90, AnalogPin.P2)
         }
     }
     basic.showNumber(CarPark)
     radio.sendNumber(CarPark)
+})
+basic.forever(function () {
+    if (CarPark < 10) {
+        if (SmartCity.read_distance_sensor(SmartCity.DistanceUnit.Centimeters, DigitalPin.P13, DigitalPin.P14) < 10) {
+            count += 1
+            basic.pause(1000)
+        } else {
+            count = 0
+        }
+        if (count == 1) {
+            CarPark += 1
+        }
+    }
 })
